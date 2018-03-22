09cb9fe0fb4acc224e6780697f8a94f698527fb8
DESKTOP-BE6T737\xiawa <xiawang1024@icloud.com>
2018-03-22 17:38:53 +0800

    微博矩阵调整

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,7 +2,11 @@
     <div class="home">
         <h1 class="title">
             河南广播电视台微博矩阵
-        </h1>        
+        </h1>     
+        <div class="hngd-weibo">
+            <img src="../logos/icon-hngd-logo.png" class="img" @click="goToQrcode(2)">
+            <p class="name">河南广播电视台</p>
+        </div>   
         <section class="hndt">
             <div class="name-box clearfix border-bottom-1px">
                 <h2 class="name-wrap right ">
@@ -57,7 +61,11 @@ export default {
         }
     },
     methods:{
-        goToQrcode(type,qrId) {             
+        goToQrcode(type,qrId) {     
+            if(type == 2) {
+                window.location.href = 'https://m.weibo.cn/u/6426290707'
+                return
+            }        
             qrId = parseInt(qrId) - 1
             let url = this.weiboLink[type][qrId].url
             if(url) {
@@ -81,11 +89,23 @@ export default {
     box-sizing border-box
     .title
         margin-top 90px
-        font-size 40px
+        text-align center
+        font-size 48px
         color #ffffff
+    .hngd-weibo
+        width 300px
+        margin 100px auto 0
+        text-align center 
+        font-size 0
+        .img
+            width 150px
+        .name
+            margin-top 28px
+            font-size 34px
+            color #ffffff
     .hndt,.hntv
         width 100%
-        margin-top 120px
+        margin-top 40px
         .name-box
             width 100%
             padding-bottom 26px
@@ -102,7 +122,7 @@ export default {
                     vertical-align bottom
                     display inline-block
                     margin-left 16px
-                    font-size 28px
+                    font-size 32px
                     color #ffffff
         .logos-wrap
             width 100%
@@ -120,7 +140,7 @@ export default {
                     padding 0 10px
                     margin-top 28px
                     line-height 1.4
-                    font-size 28px
+                    font-size 32px
                     color #ffffff
     .hntv
         .name-box
@@ -143,6 +163,6 @@ export default {
             background url('./icon-tai.png') center center no-repeat
             background-size cover
         .name-tai
-            font-size 25px
+            font-size 28px
 </style>
 
